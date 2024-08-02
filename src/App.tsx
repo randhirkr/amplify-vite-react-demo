d11b443d9b6f8753521c8055a7a06323ad7ec9db
randhirkr <cusatrandhir@gmail.com>
2024-08-02 22:09:37 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { signIn } from "aws-amplify/auth";
+import type { FormEvent } from "react"; 
+
+import { Authenticator } from '@aws-amplify/ui-react';
+import { Amplify } from 'aws-amplify';
+import '@aws-amplify/ui-react/styles.css';
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolClientId: '274lsbhjhej7g49fdqqcacnjgl',
+      userPoolId: 'ap-south-1_DEsJuPaBG',
+    }
+  }
+});
+
+export default function App() {
+  return (
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user?.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
+  );
+}
